e37428d595e2d92cdcc0d6edc886972c91990453
Joe Seago <joe.seago@gmail.com>
2016-06-14 14:18:26 -0600

    added rem buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	$(".addItem").click(function(){
-		$(".itemName").append($("#item").val())
+		var userInput = $("#item").val();
+		/* CLEAR FIELD */
+		$(".itemNameList").append('<li>' + userInput + '</li>');
+		$(".removeImg").append("<li>poop.png</li>");
+		$(".gotitImg").append("<li>Got It!</li>");
+
 	});
 
-})
+	});
+
