e096c1352f5e0a161e0a13667e428f1cb6d8d73c
Tyler van der Hoeven <hi@tyvdh.com>
2016-06-14 18:16:23 -0400

    tyler's fixes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
-	$(".addItem").click(function(){
-		var userInput = $("#item").val();
-		/* CLEAR FIELD */
-		$(".listItems").append("<li><img class='removeImg' src='images/poop.png'><span>" + userInput + "</span><img class='gotitImg' src='images/check.png'></li>");
-		/*$(".removeImg").append("<li></li>");
-		$(".gotitImg").append("<li></li>");*/
+
+	$(".textBox").submit(function(e) {
+
+    /* PREVENTS DEFAULT FORM SUBMIT ACTION */
+    e.preventDefault();
+
+		var $input = $("#input");
+
+		/* ADD NEW ITEM */
+		var $new_item = $("<li><img class='removeImg' src='images/poop.png'><span>" + $input.val() + "</span><img class='gotitImg' src='images/check.png'></li>");
+
+    /* APPENDS NEW ITEM TO UL LIST */
+    $(".listItems").append($new_item);
+
+    /* CLEARS INPUT VALUE */
+    $input.val('');
+
+    /* Strike Through Text */
+    $new_item.find('.gotitImg').on("click", function() {
+  		$(this).siblings('span').toggleClass("strikeToggle");
+  	});
+
+    /* Remove Item */
+  	$new_item.find('.removeImg').on("click", function(){
+  		$new_item.remove();
+  	});
 	});
-
-
-
-	/* Strike Through Text */
-	$("body").on("click", ".gotitImg", function(){
-		$(this).siblings().toggleClass("strikeToggle");
-	});
-
-
-	/* Remove Item */
-	$("body").on("click", ".removeImg", function(){
-		console.log($(this).siblings())
-		$(this).parent().remove();
-	});
-
 
 });
