ddaf55075137de69d84144403c04acaf7682ddc9
Eric Snell <eric_snell21@yahoo.com>
2016-06-14 19:40:36 -0400

    some styling

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
+
+	/* Add Item To List */
 	$(".addItem").click(function(){
 		var userInput = $("#item").val();
-		/* CLEAR FIELD */
 		$(".listItems").append("<li><img class='removeImg' src='images/poop.png'><span>" + userInput + "</span><img class='gotitImg' src='images/check.png'></li>");
-		/*$(".removeImg").append("<li></li>");
-		$(".gotitImg").append("<li></li>");*/
 	});
-
-
 
 	/* Strike Through Text */
 	$("body").on("click", ".gotitImg", function(){
 		$(this).siblings().toggleClass("strikeToggle");
 	});
 
-
 	/* Remove Item */
 	$("body").on("click", ".removeImg", function(){
-		console.log($(this).siblings())
 		$(this).parent().remove();
 	});
-
 
 });
